5968eb34262f5fad6446e1415c09451c0d7c10bd
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-01-29 13:22:01 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,2 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name="object-bucket",
-    description="A fun and easy way to store python objects.",
-    author="Adwaith-Rajesh",
-    author_email="adwaithrajesh3180@gmail.com",
-    url="https://github.com/Adwaith-Rajesh/object-bucket",
-    packages=find_packages(),
-)
+from setuptools import setup
+setup()
